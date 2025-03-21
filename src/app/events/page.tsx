554a23d01913d25918c1af76730970ea9a48412e
soyvillareal <sebastiangarces152@gmail.com>
2025-03-21 04:48:24 -0500

    fix: remove unused useEffect import in EventosPage component

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useEffect } from "react";
+import React from "react";
 import Section from "@/components/ui/Section";
 import { TableGeneric } from "@/components/services/TableGeneric";
 import { configEvent, configFormEvent, tableEvent } from "@/config/Events";
